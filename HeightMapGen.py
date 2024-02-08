5c7cb989ba3076e1e9b72096af32be251420ddc9
Tom Bates <merlincg46@gmail.com>
2024-02-08 20:34:43 +0000

    Configured primary and secondary noise

--- HeightMapGen.py
+++ HeightMapGen.py
@@ -7,3 +7,17 @@ plane = cmds.polyPlane(width=10, height=10, subdivisionsX=10, subdivisionsY=10)[
 noise_primary = cmds.shadingNode('noise', asShader=True, name='noise_primary')
 noise_secondary = cmds.shadingNode('noise', asShader=True, name='noise_secondary')
 
+cmds.setAttr("noise_primary.amplitude", 0.420)
+cmds.setAttr("noise_primary.ratio", 0.441)
+cmds.setAttr("noise_primary.frequencyRatio", 2.573)
+cmds.setAttr("noise_primary.depthMax", 7)
+cmds.setAttr("noise_primary.frequency", 3.497)
+cmds.setAttr("noise_primary.noiseType", 1)
+
+
+cmds.setAttr("noise_secondary.amplitude", 0.371)
+cmds.setAttr("noise_secondary.ratio", 0.343)
+cmds.setAttr("noise_secondary.frequencyRatio", 2.510)
+cmds.setAttr("noise_secondary.depthMax", 7)
+cmds.setAttr("noise_secondary.frequency", 2.797)
+cmds.setAttr("noise_secondary.noiseType", 4)
