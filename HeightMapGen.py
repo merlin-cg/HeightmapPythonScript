4f8706104fb5bd31238d8285a5f2fd745b7c07d2
Tom Bates <merlincg46@gmail.com>
2024-02-08 19:19:06 +0000

    Added file node

--- HeightMapGen.py
+++ HeightMapGen.py
@@ -4,5 +4,7 @@ import random
 
 plane = cmds.polyPlane(width=10, height=10, subdivisionsX=10, subdivisionsY=10)[0]
 
+file_texture_noise1 = cmds.shadingNode('file', asShader=True, name='file_texture_noise1')
+
 
 
