3efb300affeea3f861e56e910beb2910e94dffd4
Tom Bates <s5609424@bournemouth.ac.uk>
2024-02-19 14:22:46 +0000

    Added fuction to assign heightmap to plane in Maya

--- PerlinNoiseGen.py
+++ PerlinNoiseGen.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from opensimplex import OpenSimplex
 import math
+import platform
 
 # Heightmap size
 width = 512
@@ -48,4 +49,42 @@ heightmap = create_heightmap(width, height, seed=56456)
 plt.figure(figsize=(width / 100, height / 100)) 
 plt.imshow(heightmap, cmap='gray', interpolation='none')
 plt.axis('off')  # Turn off axis
-plt.show()
+
+plt.savefig("/home/s5609424/Pictures/heightmap.png", bbox_inches='tight', pad_inches=0)  # Save the image without extra padding
+# plt.show()
+
+noise_path = '/home/s5609424/Pictures/heightmap.png'
+
+
+# Generate plane and assign heightmap to displacement shader
+def genPlane():
+    plane = cmds.polyPlane(width=10, height=10, subdivisionsX=10, subdivisionsY=10)[0]
+    
+    lambert_shader = cmds.shadingNode('lambert', asShader=True)
+    
+    file_texture = cmds.shadingNode('file', asTexture=True, name='file_texture')
+    cmds.setAttr(file_texture + '.fileTextureName', noise_path, type="string")
+    
+    place2d_texture = cmds.shadingNode('place2dTexture', asUtility=True, name='place2d_texture')
+    
+    cmds.connectAttr(place2d_texture + '.outUV', file_texture + '.uvCoord')
+    cmds.connectAttr(place2d_texture + '.outUvFilterSize', file_texture + '.uvFilterSize')
+    
+    cmds.connectAttr(file_texture + '.outColor', lambert_shader + '.color')
+    cmds.setAttr(file_texture + '.alphaIsLuminance', 1)
+    
+    displacement_node = cmds.shadingNode('displacementShader', asShader=True)
+    
+    cmds.connectAttr(file_texture + '.outAlpha', displacement_node + '.displacement')
+    
+    shading_group = cmds.sets(renderable=True, noSurfaceShader=True, empty=True)
+    
+    cmds.connectAttr(lambert_shader + '.outColor', shading_group + '.surfaceShader')
+    cmds.connectAttr(displacement_node + '.displacement', shading_group + '.displacementShader')
+    
+    plane_shape = cmds.listRelatives(plane, shapes=True)[0]
+    cmds.sets(plane_shape, edit=True, forceElement=shading_group)
+    
+    cmds.toggleDisplacement(plane)
+    
+genPlane()
